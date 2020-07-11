e2314a299d51f68648c0854eadd339e82613cb15
Francesco Ilario <fr.ilario@gmail.com>
2020-07-11 15:22:50 +0000

    Updated protos

--- pkg/schedulerpb/scheduler.validator.pb.go
+++ pkg/schedulerpb/scheduler.validator.pb.go
@@ -5,12 +5,12 @@ package schedulerpb
 
 import (
 	fmt "fmt"
+	math "math"
 	proto "github.com/golang/protobuf/proto"
+	_ "github.com/mwitkow/go-proto-validators"
 	_ "github.com/golang/protobuf/ptypes/empty"
 	_ "github.com/golang/protobuf/ptypes/timestamp"
-	_ "github.com/mwitkow/go-proto-validators"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
